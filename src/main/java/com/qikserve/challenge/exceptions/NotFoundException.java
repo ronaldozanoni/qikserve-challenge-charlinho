8351d7431ae7a4b86a4ad186a38368cc2b0d966f
Charleston Campos <charlinhocjc@gmail.com>
2022-05-12 18:45:20 -0300

    feat(exception): create exceptions classes

---
+++ src/main/java/com/qikserve/challenge/exceptions/NotFoundException.java
@@ -0,0 +1,22 @@
+package com.qikserve.challenge.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+/**
+ * The type Not found exception.
+ */
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
+public class NotFoundException extends RuntimeException{
+
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Instantiates a new Not found exception.
+     *
+     * @param message the message
+     */
+    public NotFoundException(String message) {
+		super(message);
+	}
+}
---
+++ src/main/java/com/qikserve/challenge/exceptions/UnauthorizedException.java
@@ -0,0 +1,22 @@
+package com.qikserve.challenge.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+/**
+ * The type Unauthorized exception.
+ */
+@ResponseStatus(HttpStatus.UNAUTHORIZED)
+public class UnauthorizedException extends RuntimeException {
+
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * Instantiates a new Unauthorized exception.
+     *
+     * @param message the message
+     */
+    public UnauthorizedException(String message) {
+        super(message);
+    }
+}
